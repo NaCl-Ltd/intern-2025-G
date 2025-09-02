268c905ab3c76dc1961195794b069cc17e5bfee6
Messi0517 <hirokitch@icloud.com>
2025-09-02 13:12:02 +0900

    issue#2をマージします

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_09_02_020429) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_14_090340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,16 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_02_020429) do
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
-  create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -74,7 +64,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_02_020429) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
-    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
